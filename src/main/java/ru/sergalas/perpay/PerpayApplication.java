1cd88be259b24638a33844f75aa33ef166e50c8e
sergalas <hudos@rambler.ru>
2023-04-15 15:05:28 +0300

    add all

---
+++ src/main/java/ru/sergalas/perpay/PerpayApplication.java
@@ -0,0 +1,13 @@
+package ru.sergalas.perpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/sergalas/perpay/PerpayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sergalas.perpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
